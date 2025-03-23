f851109d2ac6293932bd4beb4ac944d5fab2f3fd
isha-web1 <isha.web1@gmail.com>
2025-03-23 16:14:11 +0600

    add database(mongoDb)& retrive data(all jobs)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 9000
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 
 
@@ -16,6 +17,43 @@ const corsOptions = {
   }
   app.use(cors(corsOptions))
   app.use(express.json())
+
+  
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qyv9war.mongodb.net/jobSpark?appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+
+    const jobsCollection = client.db('JobSpark').collection('jobs')
+    const bidsCollection = client.db('JobSpark').collection('bids')
+
+     // Get all jobs data from db
+     app.get('/jobs', async (req, res) => {
+      const result = await jobsCollection.find().toArray()
+
+      res.send(result)
+    })
+
+
+
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
   app.get('/', (req, res) => {
     res.send('Hello from SoloSphere Server....')
