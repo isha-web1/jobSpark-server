b03c496ec9a37a1463468f25e675a88c373cc7fd
isha-web1 <isha.web1@gmail.com>
2025-03-24 15:42:20 +0600

    post job & retrive all posted jobs method added

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 9000
-const { MongoClient, ServerApiVersion ,ObjectId} = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 
 
 const corsOptions = {
-    origin: [
-      'http://localhost:5173',
-      'http://localhost:5174',
-      
-    ],
-    credentials: true,
-    optionSuccessStatus: 200,
-  }
-  app.use(cors(corsOptions))
-  app.use(express.json())
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
 
-  
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+app.use(express.json())
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qyv9war.mongodb.net/jobSpark?appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -34,16 +34,16 @@ async function run() {
     const jobsCollection = client.db('JobSpark').collection('jobs')
     const bidsCollection = client.db('JobSpark').collection('bids')
 
-     // Get all jobs data from db
-     app.get('/jobs', async (req, res) => {
+    // Get all jobs data from db
+    app.get('/jobs', async (req, res) => {
       const result = await jobsCollection.find().toArray()
 
       res.send(result)
     })
 
 
-     // Get a single job data from db using job id
-     app.get('/job/:id', async (req, res) => {
+    // Get a single job data from db using job id
+    app.get('/job/:id', async (req, res) => {
       const id = req.params.id
       const query = { _id: new ObjectId(id) }
       const result = await jobsCollection.findOne(query)
@@ -51,13 +51,31 @@ async function run() {
     })
 
 
-        // Save a bid data in db
-        app.post('/bid', async (req, res) => {
-          const bidData = req.body
-    
-          const result = await bidsCollection.insertOne(bidData)
-          res.send(result)
-        })
+    // Save a bid data in db
+    app.post('/bid', async (req, res) => {
+      const bidData = req.body
+
+      const result = await bidsCollection.insertOne(bidData)
+      res.send(result)
+    })
+
+
+    // Save a job data in db
+    app.post('/job', async (req, res) => {
+      const jobData = req.body
+
+      const result = await jobsCollection.insertOne(jobData)
+      res.send(result)
+    })
+
+
+      // get all jobs posted by a specific user
+      app.get('/jobs/:email', async (req, res) => {
+        const email = req.params.email
+        const query = { 'buyer.email': email }
+        const result = await jobsCollection.find(query).toArray()
+        res.send(result)
+      })
 
 
 
@@ -73,8 +91,8 @@ async function run() {
 }
 run().catch(console.dir);
 
-  app.get('/', (req, res) => {
-    res.send('Hello from SoloSphere Server....')
-  })
-  
-  app.listen(port, () => console.log(`Server running on port ${port}`))
+app.get('/', (req, res) => {
+  res.send('Hello from SoloSphere Server....')
+})
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
